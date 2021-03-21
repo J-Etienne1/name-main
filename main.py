4cc82a3485c56bed5756ab34b9504a862aba0b0c
Jason <jasonetienne1@gmail.com>
2021-03-21 15:49:25 +0000

    .

---
+++ main.py
@@ -0,0 +1,10 @@
+######################################  NAME == MAIN  ######################################
+
+
+print('First Modules Name: {}'.format(__name__))
+
+# def main():
+#     pass
+#
+# if __name__ == '__main__':
+#     main()
