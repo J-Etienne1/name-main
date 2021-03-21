14bf926f0d1b35c69a2562ee1f3b9bf9390de798
Jason <jasonetienne1@gmail.com>
2021-03-21 16:00:26 +0000

    .

---
+++ first_module.py
@@ -0,0 +1,12 @@
+######################################  NAME == MAIN  ######################################
+
+
+
+
+# def main():
+#     print('First Modules Name: {}'.format(__name__))
+
+if __name__ == '__main__':
+    print('Run Directly')
+else:
+    print('Run from Import')
